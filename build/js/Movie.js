1f960a5fa174154a7a99835b6f70ff509567a026
tyseawood <tyseawood@protonmail.com>
2023-05-29 16:41:42 -0700

    Removed build folder and moved css to root directory

--- build/js/Movie.js
+++
@@ -1,5 +0,0 @@
-export var OriginalLanguage;
-(function (OriginalLanguage) {
-    OriginalLanguage["En"] = "en";
-    OriginalLanguage["Ja"] = "ja";
-})(OriginalLanguage || (OriginalLanguage = {}));
