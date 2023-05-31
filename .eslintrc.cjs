16178617352558626b8ba4f070e0ad4d2eddd837
tyseawood <tyseawood@protonmail.com>
2023-05-31 13:42:16 -0700

    Added prettier and eslint-prettier extention for better linting and formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  root: true
+  root: true,
 }
