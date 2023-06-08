ae00e4ba698fa8599fa2b6f687509d197aeefa30
Ty <62930969+tyseawood@users.noreply.github.com>
2023-06-08 15:43:06 -0700

    Project setup (#20)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,6 @@ module.exports = {
     'import/extensions': 0,
     'no-console': 0,
     'import/prefer-default-export': 1,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 }
